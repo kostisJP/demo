ed596c36e808a6c6d6fb6c2d8226fef5f11da209
Konstantinos Kefalas <kostis@Konstantinoss-MacBook-Pro.local>
2018-11-02 20:45:50 +0200

    Updates

---
+++ precache-manifest.fe7306d80f29b2cb1494b0b622b825a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kostisJP/demo.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6818c3a43375ddda5f56",
+    "url": "/kostisJP/demo.git/static/js/runtime~main.6818c3a4.js"
+  },
+  {
+    "revision": "5d35bf4f2576abeff153",
+    "url": "/kostisJP/demo.git/static/js/main.5d35bf4f.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/kostisJP/demo.git/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "5d35bf4f2576abeff153",
+    "url": "/kostisJP/demo.git/static/css/main.40dc9e7a.chunk.css"
+  },
+  {
+    "revision": "7ff38da95f31f5dcba23827823c1ed02",
+    "url": "/kostisJP/demo.git/index.html"
+  }
+];
